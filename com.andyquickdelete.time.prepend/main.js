12bff1b82fc56d14b0c007114f80b18bd0238ec0
AndyQuicklyDelete <dailydosha@proton.me>
2024-10-09 16:18:16 -0700

    Update

--- com.andyquickdelete.time.prepend/main.js
+++ com.andyquickdelete.time.prepend/main.js
@@ -1,44 +1,35 @@
-//const ps = require('photoshop');
 const uxp = require('uxp');
 const fs = uxp.storage.localFileSystem;
 const { app, core } = require("photoshop");
-const img = app.activeDocument;
+// const img = app.activeDocument;
 
 
 function appendTimestamp() {	
-	var now = new Date();
-	
-	year = now.getFullYear();
-	month = now.getMonth() + 1;
-	month = (month <10?'0':'') + month;
-	day = (now.getDate() <10?'0':'') + now.getDate();
-	hour = (now.getHours() <10?'0':'') + now.getHours();
-	minutes = (now.getMinutes() <10?'0':'') + now.getMinutes();
-	seconds = (now.getSeconds() <10?'0':'') + now.getSeconds();
-  
-	/*//Current format */
-	var myDate = year.toString() + "-" + month.toString() + "-" + day.toString();
-	var myTime = hour.toString() + "." + minutes.toString() + "." + seconds.toString();
-	
-	var timestamp = myDate + "_" + myTime;
-	return timestamp;
+    var now = new Date();
+
+    year = now.getFullYear();
+    month = now.getMonth() + 1;
+    month = (month <10?'0':'') + month;
+    day = (now.getDate() <10?'0':'') + now.getDate();
+    hour = (now.getHours() <10?'0':'') + now.getHours();
+    minutes = (now.getMinutes() <10?'0':'') + now.getMinutes();
+    seconds = (now.getSeconds() <10?'0':'') + now.getSeconds();
+
+    var myDate = year.toString() + "-" + month.toString() + "-" + day.toString();
+    var myTime = hour.toString() + "." + minutes.toString() + "." + seconds.toString();
+
+    var timestamp = myDate + "_" + myTime;
+    return timestamp;
 }
 
 
 async function exportMyFile() {
     await core.executeAsModal(async () => {
         try {
-            //const folder = await fs.getFolder();
+            const img = app.activeDocument;
             const folderPath = path.dirname(img.path);
-            
-            //console.log("raw path", img.path);
-            //console.log("path", folderPath);
-
             const folder = await fs.getEntryWithUrl(folderPath);
 
-            //console.log(folder);
-
-            //const file = await fs.getFileForSaving('', { types: [ 'jpg' ] });
             var myfilename = img.name;
             var myfilenameCount = myfilename.length;
             if (myfilenameCount > 19) {
